e4787d124904980ebd0b96fe674e0eeb133fed13
Julien Fontanet <julien.fontanet@isonoe.net>
2015-05-18 14:34:19 +0200

    Various updates.

--- src/index.spec.js
+++ src/index.spec.js
@@ -4,9 +4,6 @@ import myLib from './'
 
 import expect from 'must'
 
-import sourceMapSupport from 'source-map-support'
-sourceMapSupport.install()
-
 // ===================================================================
 
 describe('myLib')
