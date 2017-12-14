bcd55a4efe7a7cab72d2b86d1d3b70863e50ad1f
Julien Fontanet <julien.fontanet@isonoe.net>
2017-12-14 11:10:08 +0100

    various updates

--- .babelrc.js
+++ .babelrc.js
@@ -1,23 +1,25 @@
 const { NODE_ENV = 'development' } = process.env
 const __PROD__ = NODE_ENV === 'production'
+const __TEST__ = NODE_ENV === 'test'
 
 module.exports = {
   comments: !__PROD__,
   compact: __PROD__,
-  ignore: NODE_ENV === 'test' ? undefined : [ /\.spec\.js$/ ],
+  ignore: __TEST__ ? undefined : [ /\.spec\.js$/ ],
   plugins: ['lodash'],
   presets: [
     [
-      'env',
+      '@babel/env',
       {
-        debug: true,
+        debug: !__TEST__,
         loose: true,
+        shippedProposals: true,
         targets: {
           node: __PROD__ ? '6' : 'current'
         },
         useBuiltIns: 'usage'
       }
     ],
-    'flow'
+    '@babel/flow'
   ]
 }
